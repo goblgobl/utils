e9ef445f0964af665c00498c9c71b1254fe3539a
Karl Seguin <k@openmymind.io>
2023-02-08 19:40:33 +0800

    json marshalling for optionals

--- optional/optional.go
+++ optional/optional.go
@@ -1,12 +1,38 @@
 package optional
 
+import (
+	"bytes"
+	"encoding/json"
+)
+
 var (
-	NullInt = Null[int]()
+	NullInt    = Null[int]()
+	NullString = Null[string]()
+	jsonNull   = []byte("null")
 )
 
 type Value[T any] struct {
 	Value  T
 	Exists bool
+}
+
+func (v Value[T]) MarshalJSON() ([]byte, error) {
+	if !v.Exists {
+		return jsonNull, nil
+	}
+	return json.Marshal(v.Value)
+}
+
+func (v *Value[T]) UnmarshalJSON(data []byte) error {
+	if bytes.Equal(data, jsonNull) {
+		return nil
+	}
+
+	var value T
+	err := json.Unmarshal(data, &value)
+	v.Value = value
+	v.Exists = true
+	return err
 }
 
 func New[T any](value T) Value[T] {
@@ -19,4 +45,8 @@ func Null[T any]() Value[T] {
 
 func Int(value int) Value[int] {
 	return New[int](value)
+}
+
+func String(value string) Value[string] {
+	return New[string](value)
 }
--- optional/optional_test.go
+++ optional/optional_test.go
@@ -3,6 +3,8 @@ package optional
 import (
 	"testing"
 
+	"src.goblgobl.com/utils/json"
+
 	"src.goblgobl.com/tests/assert"
 )
 
@@ -21,4 +23,46 @@ func Test_New(t *testing.T) {
 	v := New("over")
 	assert.True(t, v.Exists)
 	assert.Equal(t, v.Value, "over")
+}
+
+func Test_Int_Json(t *testing.T) {
+	power := Int(9001)
+	data, err := json.Marshal(power)
+	assert.Nil(t, err)
+	assert.Equal(t, string(data), `9001`)
+
+	power = NullInt
+	data, err = json.Marshal(power)
+	assert.Nil(t, err)
+	assert.Equal(t, string(data), `null`)
+
+	var p1 Value[int]
+	assert.Nil(t, json.Unmarshal([]byte(`9002`), &p1))
+	assert.True(t, p1.Exists)
+	assert.Equal(t, p1.Value, 9002)
+
+	var p2 Value[int]
+	assert.Nil(t, json.Unmarshal([]byte(`null`), &p2))
+	assert.False(t, p2.Exists)
+}
+
+func Test_String_Json(t *testing.T) {
+	power := String("over")
+	data, err := json.Marshal(power)
+	assert.Nil(t, err)
+	assert.Equal(t, string(data), `"over"`)
+
+	power = NullString
+	data, err = json.Marshal(power)
+	assert.Nil(t, err)
+	assert.Equal(t, string(data), `null`)
+
+	var p1 Value[string]
+	assert.Nil(t, json.Unmarshal([]byte(`"ninet"`), &p1))
+	assert.True(t, p1.Exists)
+	assert.Equal(t, p1.Value, "ninet")
+
+	var p2 Value[string]
+	assert.Nil(t, json.Unmarshal([]byte(`null`), &p2))
+	assert.False(t, p2.Exists)
 }
